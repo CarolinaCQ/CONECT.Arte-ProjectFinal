63ec7a051aff53beee781410aac5e796a38009f6
LucasLopezd <96482300+LucasLopezd@users.noreply.github.com>
2022-06-09 19:07:15 -0300

    modificación repositorio contact

--- src/main/java/edu/team5/finalproject/repository/ContactRepository.java
+++ src/main/java/edu/team5/finalproject/repository/ContactRepository.java
@@ -1,11 +1,14 @@
 package edu.team5.finalproject.repository;
 
 import edu.team5.finalproject.entity.Contact;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ContactRepository extends JpaRepository<Contact, Long> {
 
-    // borré el método que estaba creado acá, ese método ya lo estamos heredando de JpaRepository. (findbyid)
+    Optional<Contact> findByWhatsAppNumber(Long whatsAppNumber);
 }
