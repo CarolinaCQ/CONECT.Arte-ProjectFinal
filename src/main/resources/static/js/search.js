305b411c5349b4191ef90297f358310596574b90
CarolinaCamilaQuevedo <CarolinaCQ>
2022-06-15 16:45:19 -0300

    creació búsqueda en tiempo real por nombre de grupo

--- src/main/resources/static/js/search.js
+++ src/main/resources/static/js/search.js
@@ -1,13 +1,24 @@
 const search = document.querySelectorAll('.search')[0];
 const list = document.querySelectorAll('.list')[0];
 
+
+const getGroups = async () => {
+    let response = await fetch("http://localhost:8080/get-all");
+    let data = await response.json();
+    return data;
+}
+
 function setList(groups) {
     clearList();
     for (const group of groups) {
         const item = document.createElement('li');
         item.classList.add('list-item');
+        const link = document.createElement('a');
+        link.classList.add('item-link');
+        link.setAttribute("href", `http://localhost:8080/groups/profile/${group.id}`);
         const text = document.createTextNode(group.name);
-        item.appendChild(text);
+        link.appendChild(text);
+        item.appendChild(link);
         list.appendChild(item);
     }
     if (groups.length === 0) {
@@ -43,14 +54,20 @@ function getRelevancy(value, searchTerm) {
 
 search.addEventListener('input', (event) => {
     const value = event.target.value;
-    console.log(value);
-    if (value && value.trim().length > 0) {
-        setList(groups.filter(group => {
-            return group.name.includes(value);
-        }).sort((A, B) =>{
-            return getRelevancy(B.name, value) - getRelevancy(A.name, value);
-        }));
-    } else {
-        clearList();
-    }
+
+    setTimeout(async () => {
+        if (value && value.trim().length > 0) {
+
+            const groups = await getGroups();
+
+            setList(groups.filter(group => {
+                return group.name.includes(value);
+            }).sort((A, B) => {
+                return getRelevancy(B.name, value) - getRelevancy(A.name, value);
+            }));
+        } else {
+            clearList();
+        }
+    }, 2000);
+
 })
